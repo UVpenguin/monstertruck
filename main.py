537735bfb42a81203aaad58d34bbd1ec4586e05a
Umar Naseer <umarnaseer2020@gmail.com>
2025-02-25 14:02:31 +0800

    fixed cleanup

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ in2 = 11
 in3 = 13
 in4 = 15
 
-GPIO.cleanup
+GPIO.cleanup()
 
 GPIO.setup(enA, GPIO.OUT) 
 GPIO.setup(enB, GPIO.OUT) 
@@ -23,16 +23,17 @@ GPIO.setup(in4, GPIO.OUT)
 pwmA = GPIO.PWM(enA, 255)
 pwmB = GPIO.PWM(enB, 255) 
 
-while True:
-    GPIO.output(in1, 1)
-    GPIO.output(in2, 1)
+GPIO.output(in1, 1)
+GPIO.output(in2, 1)
 
-    pwmA.start(100)
-    pwmB.start(100)
+pwmA.start(100)
+pwmB.start(100)
 
-    time.sleep(2) #Delay
+time.sleep(2) #Delay
 
-    GPIO.output(in1, 0)
-    GPIO.output(in2, 0)
+GPIO.output(in1, 0)
+GPIO.output(in2, 0)
 
-    time.sleep(2) #Delay
+time.sleep(2) #Delay
+
+GPIO.cleanup()
