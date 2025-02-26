3519082e2ba44a7cbb2e96e25721a46ded1a1ec1
Umar Naseer <umarnaseer2020@gmail.com>
2025-02-26 11:23:52 +0800

    test: changed pwm freq

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ GPIO.setup(in2, GPIO.OUT)
 GPIO.setup(in3, GPIO.OUT) 
 GPIO.setup(in4, GPIO.OUT) 
 
-pwmA = GPIO.PWM(enA, 100000)
-pwmB = GPIO.PWM(enB, 100000)
+pwmA = GPIO.PWM(enA, 10000)
+pwmB = GPIO.PWM(enB, 10000)
 
 pwmA.start(100)
 pwmB.start(100)
