ce270c4e461682bb78b3d230d5808680cf3d79c5
Umar Naseer <umarnaseer2020@gmail.com>
2025-02-27 11:36:33 +0800

    added keyboard controls

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 import movement as motor
+import keyboard
 
 GPIO.cleanup()
 
-GPIO.setmode(GPIO.BOARD) 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 enA = motor.enA
@@ -15,11 +16,11 @@ in3 = motor.in3
 in4 = motor.in4
 
 GPIO.setup(enA, GPIO.OUT)
-GPIO.setup(enB, GPIO.OUT) 
-GPIO.setup(in1, GPIO.OUT) 
-GPIO.setup(in2, GPIO.OUT) 
-GPIO.setup(in3, GPIO.OUT) 
-GPIO.setup(in4, GPIO.OUT) 
+GPIO.setup(enB, GPIO.OUT)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
 
 pwmA = GPIO.PWM(enA, 1000)
 pwmB = GPIO.PWM(enB, 1000)
@@ -27,9 +28,23 @@ pwmB = GPIO.PWM(enB, 1000)
 pwmA.start(100)
 pwmB.start(100)
 
-motor.left()
-time.sleep(1)
+try:
+    while True:
+        userInput = input()
 
-motor.stop()
-pwmA.stop()
-pwmB.stop()
+        if userInput == "w":
+            motor.forward()
+        if userInput == "s":
+            motor.backward()
+        if userInput == "d":
+            motor.right()
+        if userInput == "a":
+            motor.left()
+        if userInput == "g":
+            motor.right90()
+        if userInput == "f":
+            motor.left90()
+except KeyboardInterrupt:
+    motor.stop()
+    pwmA.stop()
+    pwmB.stop()
