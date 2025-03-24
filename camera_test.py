a41847a05b77fc41f0e7944f36874e7e35267915
Umar Naseer <umarnaseer2020@gmail.com>
2025-03-24 09:57:17 +0800

    changed camera test to opencv

--- camera_test.py
+++ camera_test.py
@@ -1,12 +1,25 @@
-import time
+import numpy as np
+import cv2 as cv
 
-from picamera2 import Picamera2, Preview
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
 
-preview_config = picam2.create_preview_configuration()
-picam2.configure(preview_config)
-
-picam2.start()
-time.sleep(5)
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
