90089cc4db8434148e65d3b5a232bf9103829119
Umar Naseer <umarnaseer2020@gmail.com>
2025-03-24 10:42:13 +0800

    updates frame data continuously

--- camera_test.py
+++ camera_test.py
@@ -5,8 +5,12 @@ picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration())
 picam2.start()
 
-frame = picam2.capture_array()
+while True:
+    frame = picam2.capture_array()
+    cv2.imshow("Camera Feed", frame)
 
-cv2.imshow("Camera Feed", frame)
-cv2.waitKey(0)
+    # Press 'q' to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 cv2.destroyAllWindows()
