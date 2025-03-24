592c865395d57c1c8fde2c5cbacd27e78bc93efa
Umar Naseer <umarnaseer2020@gmail.com>
2025-03-24 10:02:34 +0800

    update camera test

--- camera_test.py
+++ camera_test.py
@@ -1,25 +1,11 @@
-import numpy as np
-import cv2 as cv
+import time
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+from picamera2 import Picamera2, Preview
 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
+picam2 = Picamera2()
 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+preview_config = picam2.create_preview_configuration()
+picam2.configure(preview_config)
+
+picam2.start()
+time.sleep(5)
