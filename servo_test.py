6b6436d483d6319625368a604363be04614a8be9
Umar Naseer <umarnaseer2020@gmail.com>
2025-03-26 12:49:53 +0800

    added pigpio

--- servo_test.py
+++ servo_test.py
@@ -1,12 +1,7 @@
+from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import Servo
 
+factory = PiGPIOFactory()
+s = Servo("BOARD35", pin_factory=factory)
 
-# GPIO SETUP
-s = Servo("BOARD35")
-
-
-try:
-    s.mid()
-
-finally:
-    pass
+s.mid()
