f0d9191a6ebaa4b4f38573cd7db2a8bf7115cc8e
Umar Naseer <umarnaseer2020@gmail.com>
2025-03-26 12:56:09 +0800

    changed test

--- servo_test.py
+++ servo_test.py
@@ -1,7 +1,12 @@
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import Servo
+from gpiozero.tools import sin_values
+from signal import pause
 
 factory = PiGPIOFactory()
 s = Servo("BOARD35", pin_factory=factory)
 
-s.max()
+s.source = sin_values()
+s.source_delay = 0.1
+
+pause()
