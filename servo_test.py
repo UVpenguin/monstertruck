f5e4a5877080feb76b878671bd7ed0c89aba5e19
Umar Naseer <umarnaseer2020@gmail.com>
2025-03-26 13:03:51 +0800

    changed test

--- servo_test.py
+++ servo_test.py
@@ -3,16 +3,18 @@ from gpiozero import AngularServo
 from time import sleep
 
 factory = PiGPIOFactory()
-s = AngularServo("BOARD35", pin_factory=factory, min_angle=-90, max_angle=90)
+s = AngularServo("BOARD35", pin_factory=factory, min_angle=-180, max_angle=180)
 
 while True:
+    s.angle = -180
+    sleep(2)
     s.angle = -90
     sleep(2)
     s.angle = -45
     sleep(2)
     s.angle = 0
     sleep(2)
-    s.angle = 45
-    sleep(2)
     s.angle = 90
+    sleep(2)
+    s.angle = 180
     sleep(2)
