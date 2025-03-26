8ae3c6a2eee0e87aac10796f78ee1d795a12a169
Umar Naseer <umarnaseer2020@gmail.com>
2025-03-26 11:52:18 +0800

    removed calibration and added servo test

--- servo_cal.py
+++
@@ -1,8 +0,0 @@
-from gpiozero import Servo
-import RPi.GPIO as GPIO
-
-GPIO.cleanup()
-
-s = Servo(13)
-print(s.min())
-print(s.max())
---
+++ servo_test.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+servo_pin = 33
+
+# GPIO setup
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+
+pwm = GPIO.PWM(servo_pin, 50)
+pwm.start(0)
+
+
+def set_angle(angle):
+    duty_cycle = (angle / 18) + 2
+    GPIO.output(servo_pin, True)
+    pwm.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)
+    GPIO.output(servo_pin, False)
+    pwm.ChangeDutyCycle(0)
+
+
+try:
+    while True:
+        set_angle(0)
+        time.sleep(2)
+        set_angle(90)
+        time.sleep(2)
+        set_angle(180)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
