3b076083c8c39b00940a9ac92d6052c067b6398a
Umar Naseer <umarnaseer2020@gmail.com>
2025-03-28 12:26:04 +0800

    added semi circle

--- pattern3.py
+++ pattern3.py
@@ -35,7 +35,7 @@ def detect_shape(contour):
                 if 2 <= defects.shape[0] <= 3:
                     shape = "arrow"
                 else:
-                    shape = "circle"
+                    shape = "semi circle"
             else:
                 shape = "circle"
         else:
@@ -207,7 +207,7 @@ while True:
                 cv2.putText(
                     frame,
                     f"{label}",
-                    (cX - 50, cY +20),
+                    (cX - 50, cY + 20),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     0.6,
                     (0, 0, 255),
