857468f3a2b5e3ddd6e8800ded9da2bebf0dd9e4
Umar Naseer <umarnaseer2020@gmail.com>
2025-04-22 11:26:53 +0800

    refactor: update template loading to include sizes for improved feature matching

--- feature_match.py
+++ feature_match.py
@@ -4,7 +4,7 @@ import numpy as np
 import utility
 
 # load templates once
-templ_kps, templ_des, names = utility.loadTemplates()
+tmpl_kps, tmpl_des, tmpl_sizes, names = utility.loadTemplates()
 
 # configure PiCamera2
 picam2 = Picamera2()
