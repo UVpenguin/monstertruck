fa5d7be91bb7f37fca9dff51d94a921da5a332a8
Umar Naseer <umarnaseer2020@gmail.com>
2025-04-22 14:19:03 +0800

    fix: adjust threshold value in feature matching process

--- feature_match.py
+++ feature_match.py
@@ -93,7 +93,7 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    _, thresh = cv2.threshold(blur, 80, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     thresh = cv2.morphologyEx(
         thresh, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8), iterations=2
     )
